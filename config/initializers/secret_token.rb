6ed816edce8f58f2942508d996ac44ccee1f5300
Ricardo Lopes <mail@ricardolopes.net>
2012-04-12 11:08:59 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Klinkr::Application.config.secret_token = 'e05bfc33d1ce928f47f681cbe47c5fff9585f0f90903a535207bc42227c02196c52f640283111d808fdc60a48b1a48a625353caf736a2579ec9bf1525bd75bd3'
